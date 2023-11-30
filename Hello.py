0d214e41525fbc83740d72a451b15986f46c52c7
marikolk <152090034+marikolk@users.noreply.github.com>
2023-11-30 11:15:54 +0000

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -49,3 +49,63 @@ def run():
 
 if __name__ == "__main__":
     run()
+
+def run():
+  
+  st.title("Vaccination app")
+  
+  user_type = st.radio("Select your user type:", ["Hospital", "Government"])
+  
+  
+  if user_type == "Hospital":
+    st.title("Hospital Vaccination App")
+  elif user_type == "Government":
+    st.title("Government Vaccination App")
+
+if __name__ == "__main__":
+    run()
+  
+def hospital_menu():
+    st.write("\nHospital Menu:")
+    choice = st.selectbox("Select an option:", ["Get a Report", "Add a Patient", "Update Vaccine Status"])
+  
+    if choice == "Get a Report":
+        st.write("Hospital: Getting a report...")
+    elif choice == "Add a Patient":
+        st.write("Hospital: Adding a patient...")
+    elif choice == "Update Vaccine Status":
+        st.write("Hospital: Updating vaccine status...")
+  
+  
+def government_menu():
+    st.write("\nGovernment Menu:")
+    choice = st.selectbox("Select an option:", ["Add Citizens", "Distribute Vaccines", "Get Overall Report"])
+  
+    if choice == "Add Citizens":
+        st.write("Government: Adding citizens...")
+    elif choice == "Distribute Vaccines":
+        st.write("Government: Distributing vaccines...")
+    elif choice == "Get Overall Report":
+        st.write("Government: Getting overall report...")
+
+def run():
+
+  st.title("Vaccination app")
+  
+  user_type = st.radio("Select your user type:", ["Hospital", "Government"])
+  
+  if user_type == "Hospital":
+      st.title("Hospital Vaccination App")
+  
+      # Add a button to proceed after selecting user type
+      if st.button("Continue"):
+          # Call the hospital_menu function
+          hospital_menu()
+  
+  elif user_type == "Government":
+      st.title("Government Vaccination App")
+  
+      # Add a button to proceed after selecting user type
+      if st.button("Continue"):
+          # Call the government_menu function
+          government_menu()
